74476cd51d637f6a5f4a3d0a8eb5313d5f8c13d1
mellava <6126925+mellava@users.noreply.github.com>
2023-04-01 14:07:45 -0700

    Add alternate solution

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,4 +1,4 @@
-var Stack = function() {
+/*var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -17,6 +17,38 @@ var Stack = function() {
 
   someInstance.size = function() {
     return Object.keys(storage).length;
+  };
+
+  return someInstance;
+};
+*/
+
+var Stack = function() {
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var size = 0;
+  // Implement the methods below
+  someInstance.push = function(value) {
+    size++;
+    storage[size] = value;
+  };
+
+  someInstance.pop = function() {
+    if (size === 0) {
+      return;
+    } else {
+      var temp = storage[size];
+      delete storage[size];
+      size--;
+      return temp;
+    }
+  };
+
+
+  someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
